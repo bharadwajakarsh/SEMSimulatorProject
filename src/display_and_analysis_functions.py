ce9ca43a2463ea8c94a15d0d48a2ed8df1b76218
bharadwajakarsh <akarshsbharadwaj@gmail.com>
2024-06-25 13:50:38 +0200

    Removed gaussian blurring of mask. Not useful at this point.

--- src/display_and_analysis_functions.py
+++ src/display_and_analysis_functions.py
@@ -66,16 +66,7 @@ def display_mask(sparseImageObject: SparseImage, originalImageObject: SEMImage):
     plt.show()
 
 
-def display_stitched_image(lowDTImageObject, highDTImageObject, sparsityPercent, stitchType, kernelSize=None):
-    if stitchType == 'gaussian':
-        if not kernelSize:
-            raise ValueError('Must specify kernel size for gaussian-stitching method')
-        if not isinstance(kernelSize, int):
-            raise ValueError("Kernel size must be an integer.")
-        if kernelSize <= 0:
-            raise ValueError("Kernel size must be a positive integer.")
-        if kernelSize % 2 == 0:
-            raise ValueError("Kernel size must be an odd integer.")
+def display_stitched_image(lowDTImageObject, highDTImageObject, sparsityPercent):
 
     plt.figure()
     plt.imshow(lowDTImageObject.extractedImage, cmap='grey')
@@ -87,35 +78,17 @@ def display_stitched_image(lowDTImageObject, highDTImageObject, sparsityPercent,
     plt.title("High DT Image")
     plt.show()
 
-    if stitchType == 'gaussian':
-        stitchedImageGauss = stitch_with_gaussian_blur(lowDTImageObject, highDTImageObject, sparsityPercent, kernelSize)
-        plt.figure()
-        plt.title("Gaussian stitching, dwell-times: {}".format([lowDTImageObject.dwellTime,
-                                                                highDTImageObject.dwellTime]))
-        plt.imshow(stitchedImageGauss, cmap='grey')
-        plt.show()
-    elif stitchType == 'normal':
-        stitchedImageNormal = stitch_images(lowDTImageObject, highDTImageObject, sparsityPercent)
-        plt.figure()
-        plt.title("Normal stitching, dwell-times: {}".format([lowDTImageObject.dwellTime, highDTImageObject.dwellTime]))
-        plt.imshow(stitchedImageNormal, cmap='grey')
-        plt.show()
+    stitchedImageNormal = stitch_images(lowDTImageObject, highDTImageObject, sparsityPercent)
+    plt.figure()
+    plt.title("Normal stitching, dwell-times: {}".format([lowDTImageObject.dwellTime, highDTImageObject.dwellTime]))
+    plt.imshow(stitchedImageNormal, cmap='grey')
+    plt.show()
 
 
 def calculate_psnr(originalImage, hybridImage):
     if np.linalg.norm(originalImage - hybridImage) == 0:
         return float('inf')
     return -10 * np.log10(np.mean((originalImage - hybridImage) ** 2))
-
-
-def compare_stitching_methods(lowDTImageObject, highDTImageObject, sparsityPercent, kernelSize):
-    lowDTImage = np.copy(lowDTImageObject.extractedImage)
-    stitchedImageNormal = stitch_images(lowDTImageObject, highDTImageObject, sparsityPercent)
-    stitchedImageGauss = stitch_with_gaussian_blur(lowDTImageObject, highDTImageObject, sparsityPercent, kernelSize)
-    psnrNormalStitch = calculate_psnr(lowDTImage, stitchedImageNormal)
-    psnrGaussianStitch = calculate_psnr(lowDTImage, stitchedImageGauss)
-    return psnrGaussianStitch / psnrNormalStitch
-
 
 """
 Execution 
--- src/stitch_images.py
+++ src/stitch_images.py
@@ -36,35 +36,3 @@ def stitch_images(lowDTImageObject, highDTImageObject, sparsityPercent):
 
     return stitchedImageFlat.reshape(lowDTImageObject.extractedImage.shape)
 
-
-def stitch_with_gaussian_blur(lowDTImageObject, highDTImageObject, sparsityPercent, kernelSize):
-    if not isinstance(lowDTImageObject, SEMImage):
-        raise ValueError("Image is not of type SEM Object")
-    if not isinstance(highDTImageObject, SEMImage):
-        raise ValueError("Image is not of type SEM Object")
-    if lowDTImageObject.dwellTime > highDTImageObject.dwellTime:
-        raise ValueError("First image should be of lower dwell-time")
-    if sparsityPercent < 0 or sparsityPercent > 100:
-        raise ValueError("illegal sparsity percentage")
-
-    stitchedImage = np.copy(lowDTImageObject.extractedImage)
-    highDTImage = highDTImageObject.extractedImage
-
-    gradientsLowDTImage = compute_image_of_relative_gradients(stitchedImage)
-    impPixelCoords = detect_sharp_edges_indices(gradientsLowDTImage, sparsityPercent)
-
-    kernelOneD = get_numpy_gaussian_kernel(kernelSize, 0.1)
-    kernelTwoD = np.outer(kernelOneD, kernelOneD.T)
-    kernelTwoDFlat = np.ravel(kernelTwoD)
-
-    maskToSee = np.zeros(stitchedImage.size)
-    maskToSee[impPixelCoords] = calculate_pixel_interests(gradientsLowDTImage, impPixelCoords)
-
-    blurdMask = np.convolve(maskToSee, kernelTwoDFlat, mode='same').reshape(stitchedImage.shape)
-
-    for i in range(len(blurdMask)):
-        for j in range(len(blurdMask)):
-            if blurdMask[i, j] != 0:
-                stitchedImage[i, j] = highDTImage[i, j]
-
-    return stitchedImage

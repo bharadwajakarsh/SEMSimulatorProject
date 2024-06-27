3c409a94be691734a46d669ee7e7130d1f7e2704
bharadwajakarsh <akarshsbharadwaj@gmail.com>
2024-06-27 14:21:36 +0200

    Updated tests

--- tests/test_stitch_images.py
+++ tests/test_stitch_images.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from src.display_and_analysis_functions import calculate_psnr
 from src.initialize_database import SEMImage
-from src.stitch_images import stitch_images, stitch_with_gaussian_blur
+from src.stitch_images import stitch_images
 
 
 class TestStitchImages(unittest.TestCase):
@@ -21,21 +21,6 @@ class TestStitchImages(unittest.TestCase):
         expectedStitchedImage = np.array(
             [[0, 20, 0, 0, 0], [0, 0, 10, 0, 0], [0, 0, 0, 1, 0], [0, 5, 0, 165, 1], [2, 0, 0, 1, 0]]).astype(float)
         stitchedImageForTest = stitch_images(exampleLDTImage, exampleHDTImage, 15)
-
-        self.assertEqual(0, np.linalg.norm(expectedStitchedImage - stitchedImageForTest))
-
-    def test_stitch_with_gaussian_blur(self):
-        exampleLDTImage = SEMImage(20, 5, np.zeros((5, 5), dtype=float))
-        exampleLDTImage.extractedImage[0, 1] = 20
-        exampleLDTImage.extractedImage[3, 1] = 5
-        exampleLDTImage.extractedImage[1, 2] = 10
-        exampleLDTImage.extractedImage[4, 0] = 2
-        exampleLDTImage.extractedImage[3, 3] = 165
-        exampleHDTImage = SEMImage(200, 5, np.ones((5, 5), dtype=float))
-
-        expectedStitchedImage = np.array(
-            [[0, 20, 0, 0, 0], [0, 0, 10, 0, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]).astype(float)
-        stitchedImageForTest = stitch_with_gaussian_blur(exampleLDTImage, exampleHDTImage, 15, 3)
 
         self.assertEqual(0, np.linalg.norm(expectedStitchedImage - stitchedImageForTest))
 
